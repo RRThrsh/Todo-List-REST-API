0a9c5c0d800b6d82417645d652439d66c0f7bc66
Yhaanxi <Ucyancy@gmail.com>
2026-04-08 22:12:59 +0800

    FEAT: add todoRoutes

---
+++ routes/todoRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const todoController = require('../controllers/todoController');
+const validateTodo = require('../middleware/validateTodo');
+
+// Routes
+router.get('/', todoController.getTodos);
+router.post('/', validateTodo, todoController.addTodo);
+router.put('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
+
+module.exports = router;
